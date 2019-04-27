01f9a425396b9f771f2d60c9febacd671a406b6e
Zen4All <info@zen4all.nl>
2019-04-27 22:37:34 +0200

    updates

--- 2_Modified_Core_Files/YOUR_ADMIN/includes/languages/english/orders.php
+++ 2_Modified_Core_Files/YOUR_ADMIN/includes/languages/english/orders.php
@@ -1,32 +1,34 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2007 Zen Cart Development Team
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: orders.php 6214 2007-04-17 02:24:25Z ajeh $
+ * @version $Id: lat9 Fri Nov 9 08:33:17 2018 -0500 Modified in v1.5.6 $
  */
-//////////////////////////////////////////////////////////////////////////
-//  Based on Super Order 2.0                                        	//
-//  By Frank Koehl - PM: BlindSide (original author)                	//
-//                                                                  	//
-//  Super Orders Updated by:						//
-//  ~ JT of GTICustom							//
-//  ~ C Jones Over the Hill Web Consulting (http://overthehillweb.com)	//
-//  ~ Loose Chicken Software Development, david@loosechicken.com	//
-//                                                      		//
-//  Powered by Zen-Cart (www.zen-cart.com)              		//
-//  Portions Copyright (c) 2005 The Zen-Cart Team       		//
-//////////////////////////////////////////////////////////////////////////
-//  DESCRIPTION:  Language file definitions for super_orders.php	//
-//////////////////////////////////////////////////////////////////////////
-
-require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . 'order_status_email.php');
+/**
+ *  Based on Super Order 2.0
+ *  By Frank Koehl - PM: BlindSide (original author)
+ *
+ *  Super Orders Updated by:
+ *  ~ JT of GTICustom
+ *  ~ C Jones Over the Hill Web Consulting (http://overthehillweb.com)
+ *  ~ Loose Chicken Software Development, david@loosechicken.com	//
+ *
+ *  Powered by Zen-Cart (www.zen-cart.com)
+ *  Portions Copyright (c) 2005 The Zen-Cart Team
+ *
+ *  DESCRIPTION:  Language file definitions for super_orders.php
+ */
 
 define('HEADING_TITLE', 'Orders');
+define('HEADING_TITLE_DETAILS', 'Order Details');
 define('HEADING_TITLE_SEARCH', 'Order ID:');
 define('HEADING_TITLE_STATUS', 'Status:');
-define('HEADING_TITLE_SEARCH_DETAIL_ORDERS_PRODUCTS', 'Search by Product Name or <strong>ID:XX</strong> or Model ');
+define('HEADING_TITLE_SEARCH_DETAIL_ORDERS_PRODUCTS', 'Product Name or ID:XX or Model');
+define('HEADING_TITLE_SEARCH_ALL','Search: ');
+define('HEADING_TITLE_SEARCH_PRODUCTS','Product search: ');
+define('TEXT_RESET_FILTER', 'Remove search filter');
 define('TEXT_INFO_SEARCH_DETAIL_FILTER_ORDERS_PRODUCTS', 'Search Filter: ');
 define('TABLE_HEADING_PAYMENT_METHOD', 'Payment<br />Shipping');
 define('TABLE_HEADING_ORDERS_ID','ID');
@@ -45,22 +47,22 @@ define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
 define('TABLE_HEADING_PRODUCTS', 'Products');
 define('TABLE_HEADING_TAX', 'Tax');
 define('TABLE_HEADING_TOTAL', 'Total');
-//Begin Edit Orders Changes 1 of 5
 define('TABLE_HEADING_PRICE_EXCLUDING_TAX', 'Price (excl)');
 define('TABLE_HEADING_PRICE_INCLUDING_TAX', 'Price (incl)');
 define('TABLE_HEADING_TOTAL_EXCLUDING_TAX', 'Total (excl)');
 define('TABLE_HEADING_TOTAL_INCLUDING_TAX', 'Total (incl)');
-//End Edit Orders Changes 1 of 5
+define('TABLE_HEADING_UPDATED_BY', 'Updated By');
 
 define('TABLE_HEADING_CUSTOMER_NOTIFIED', 'Customer Notified');
 define('TABLE_HEADING_DATE_ADDED', 'Date Added');
 
 define('ENTRY_CUSTOMER', 'Customer:');
+define('ENTRY_CUSTOMER_ADDRESS', 'Customer Address:<br><i class="fa fa-2x fa-user"></i>');
 define('ENTRY_SOLD_TO', 'SOLD TO:');
 define('ENTRY_DELIVERY_TO', 'Delivery To:');
 define('ENTRY_SHIP_TO', 'SHIP TO:');
-define('ENTRY_SHIPPING_ADDRESS', 'Shipping Address:');
-define('ENTRY_BILLING_ADDRESS', 'Billing Address:');
+define('ENTRY_SHIPPING_ADDRESS', 'Shipping Address:<br><i class="fa fa-2x fa-truck"></i>');
+define('ENTRY_BILLING_ADDRESS', 'Billing Address:<br><i class="fa fa-2x fa-credit-card"></i>');
 define('ENTRY_PAYMENT_METHOD', 'Payment Method:');
 define('ENTRY_CREDIT_CARD_TYPE', 'Credit Card Type:');
 define('ENTRY_CREDIT_CARD_OWNER', 'Credit Card Owner:');
@@ -72,17 +74,13 @@ define('ENTRY_TAX', 'Tax:');
 define('ENTRY_SHIPPING', 'Shipping:');
 define('ENTRY_TOTAL', 'Total:');
 define('ENTRY_DATE_PURCHASED', 'Date Purchased:');
-//Begin Edit Orders Changes 2 of 5
-define('ENTRY_STATUS', 'Update Status:');
+define('ENTRY_STATUS', 'Status:');
 define('ENTRY_DATE_LAST_UPDATED', 'Date Last Updated:');
-define('ENTRY_NOTIFY_CUSTOMER', 'Notify Customer?');
-define('ENTRY_NOTIFY_COMMENTS', 'Append Comments?');
-//End Edit Orders Changes 2 of 5
+define('ENTRY_NOTIFY_CUSTOMER', 'Notify Customer:');
+define('ENTRY_NOTIFY_COMMENTS', 'Append Comments:');
 define('ENTRY_PRINTABLE', 'Print Invoice');
 
-//Begin Edit Orders Changes 3 of 5
-define('TEXT_INFO_HEADING_DELETE_ORDER', 'Delete Order - ');
-//End Edit Orders Changes 3 of 5
+define('TEXT_INFO_HEADING_DELETE_ORDER', 'Delete Order');
 define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete this order?');
 define('TEXT_INFO_RESTOCK_PRODUCT_QUANTITY', 'Restock product quantity');
 define('TEXT_DATE_ORDER_CREATED', 'Date Created:');
@@ -92,7 +90,6 @@ define('TEXT_PAID', 'Paid');
 define('TEXT_UNPAID', 'Un-paid');
 
 define('TEXT_ALL_ORDERS', 'All Orders');
-define('TEXT_NO_ORDER_HISTORY', 'No Order History Available');
 
 define('EMAIL_SEPARATOR', '------------------------------------------------------');
 define('EMAIL_TEXT_SUBJECT', 'Order Update');
@@ -108,9 +105,7 @@ define('ERROR_ORDER_DOES_NOT_EXIST', 'Error: Order does not exist.');
 define('SUCCESS_ORDER_UPDATED', 'Success: Order has been successfully updated.');
 define('WARNING_ORDER_NOT_UPDATED', 'Warning: Nothing to change. The order was not updated.');
 
-//Begin Edit Orders Changes 4 of 5
-define('ENTRY_ORDER_ID','Order #');
-//End Edit Orders Changes 4 of 5
+define('ENTRY_ORDER_ID','Invoice No. ');
 define('TEXT_INFO_ATTRIBUTE_FREE', '&nbsp;-&nbsp;<span class="alert">FREE</span>');
 
 define('TEXT_DOWNLOAD_TITLE', 'Order Download Status');
@@ -143,7 +138,18 @@ define('TABLE_HEADING_CUSTOMER_COMMENTS', 'Customer<br />Comments');
 define('TEXT_COMMENTS_YES', 'Customer Comments - YES');
 define('TEXT_COMMENTS_NO', 'Customer Comments - NO');
 
-// BEGIN SUPER ORDERS  Changes 5 of 5 ----------------------------------------------
+define('TEXT_CUSTOMER_LOOKUP', '<i class="fa fa-search"></i> Lookup Customer');
+
+define('TEXT_INVALID_ORDER_STATUS', '<span class="alert">(Invalid Order Status)</span>');
+
+define('BUTTON_TO_LIST', 'Order List');
+define('SELECT_ORDER_LIST', 'Jump to Order:');
+
+define('TEXT_MAP_CUSTOMER_ADDRESS', 'Map Customer Address');
+define('TEXT_MAP_SHIPPING_ADDRESS', 'Map Shipping Address');
+define('TEXT_MAP_BILLING_ADDRESS', 'Map Billing Address');
+
+// BEGIN SUPER ORDERS  Changes 1 of 1
 define('HEADING_TITLE_ORDERS_LISTING', 'Orders Listing');
 define('HEADING_TITLE_ORDER_DETAILS', 'Order # ');
 
@@ -185,7 +191,7 @@ define('WARNING_ORDER_REOPEN', 'Warning: Order #%s has been re-opened');
 
 define('TEXT_NEW_WINDOW', ' (New Window)');
 define('IMAGE_SHIPPING_LABEL', 'Shipping Label');
-define('IMAGE_ORDER_DETAILS', 'Display Order Details'); 
+define('IMAGE_ORDER_DETAILS', 'Display Order Details');
 define('ICON_ORDER_DETAILS', 'Display Order Details');
 define('ICON_ORDER_PRINT', 'Print Data Sheet' . TEXT_NEW_WINDOW);
 define('ICON_ORDER_INVOICE', 'Display Invoice' . TEXT_NEW_WINDOW);
@@ -198,7 +204,7 @@ define('ICON_EDIT_HISTORY', 'Edit Hidden (Admin) Comments');
 define('ICON_CLOSE_STATUS', 'Close Status');
 define('ICON_MARK_COMPLETED', 'Mark Order Completed');
 define('ICON_MARK_CANCELLED', 'Mark Order Cancelled');
-define('ICON_ORDER_EDIT', 'Edit this Order'); 
+define('ICON_ORDER_EDIT', 'Edit this Order');
 
 define('SUPER_IMAGE_ORDER_PRINT', 'Print Data Sheet' . TEXT_NEW_WINDOW);
 define('SUPER_IMAGE_ORDERS_INVOICE', 'Display Invoice' . TEXT_NEW_WINDOW);
@@ -227,18 +233,4 @@ define('TEXT_PAYMENT_DATA', 'Order Payment Data');
 define('TEXT_MAILTO', 'mailto');
 define('TEXT_STORE_EMAIL', 'web');
 define('TEXT_WHOIS_LOOKUP', 'whois');
-
-define('BUTTON_TO_LIST', 'Order List');
-define('SELECT_ORDER_LIST', 'Jump to Order:');
-
-// END SUPER ORDERS  Changes 5 of 5----------------------------------------------
-
-// TY TRACKER 1 BEGIN  ----------------------------------------------
-define('TABLE_HEADING_TRACKING_ID', 'Tracking ID');
-define('TABLE_HEADING_CARRIER_NAME', 'Carrier');
-define('ENTRY_ADD_TRACK', 'Add Tracking ID');
-define('IMAGE_TRACK', 'Add Tracking ID');
-//define('BUTTON_TO_LIST', 'Order List');
-//define('SELECT_ORDER_LIST', 'Jump to Order: ');
-define('EMAIL_TEXT_COMMENTS_TRACKING_UPDATE', '<em>Items from your order will be shipping soon!</em>'); 
-// END TY TRACKER 1 -------------------------------------------------
+// END SUPER ORDERS  Changes 1 of 1
